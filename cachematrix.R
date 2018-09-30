19cc94ceab299f8cc2e8e6adde4a59edd30ef27d
KPrv <kaiareet@msn.com>
2018-09-30 14:36:36 +0800

    https://github.com/KaiaR/ProgrammingAssignment2.git

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow to save processing power for certain memory consuming computations in R
+## by allowing to save the result to cache for matrices that value does not change
 
-## Write a short comment describing this function
+
+## The first function creates a vector that is function of setting a value of the matrix, getting a value of the matrix, 
+## setting an inverse of a matrix and getting an inverse of a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The second function returns the value of inverse of x using previously calculated value saved as cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("Getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  m
 }
